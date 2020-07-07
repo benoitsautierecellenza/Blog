58a6fe43b3c7f8f2c42c25b12e016537ff576b92
Benoît SAUTIERE <39596158+Benoitsautierecellenza@users.noreply.github.com>
2020-07-07 12:58:59 +0200

    Update installaz.ps1

--- installaz.ps1
+++ installaz.ps1
@@ -2,3 +2,4 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -name PsGallery -InstallationPolicy Trusted
 Install-Module -Name PowerShellGet -Force
 Install-Module -Name Az -AllowClobber 
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
