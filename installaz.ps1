602496255df0ac5186eb2cb67280e601c6ceb2f5
Benoît SAUTIERE <39596158+Benoitsautierecellenza@users.noreply.github.com>
2020-07-07 11:12:13 +0200

    Update installaz.ps1

--- installaz.ps1
+++ installaz.ps1
@@ -1,4 +1,4 @@
-Set-PSRepository -InstallationPolicy Trusted
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Set-PSRepository -name PsGallery -InstallationPolicy Trusted
 Install-Module -Name PowerShellGet -Force
 Install-Module -Name Az -AllowClobber 
-
